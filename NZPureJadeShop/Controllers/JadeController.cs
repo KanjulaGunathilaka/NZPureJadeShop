6353070188263577506df1ce83b19ee28b7faa15
Kanjula Gunathilaka <ksgunathilaka071@gmail.com>
2024-05-24 22:45:37 +1200

    View components and Custom TagHelpers  Added

--- NZPureJadeShop/Controllers/JadeController.cs
+++ NZPureJadeShop/Controllers/JadeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NZPureJadeShop.Models;
 using NZPureJadeShop.Models.IRepository;
 using NZPureJadeShop.ViewModels;
+using System.IO.Pipelines;
 
 namespace NZPureJadeShop.Controllers
 {
@@ -14,14 +16,33 @@ namespace NZPureJadeShop.Controllers
             _jadeRepository = jadeRepository;
             _categoryRepository = categoryRepository;
         }
-        public IActionResult List()
+        //public IActionResult List()
+        //{
+        //    //return View(_jadeRepository.AllJades);
+
+        //    JadeListViewModel jadeListViewModel = new JadeListViewModel(_jadeRepository.AllJades, "All Jades");
+        //    return View(jadeListViewModel);
+
+        //}
+
+        public ViewResult List(string category)
         {
-            //return View(_jadeRepository.AllJades);
+            IEnumerable<Jade> jades;
+            string? currentCategory;
 
-            JadeListViewModel jadeListViewModel = new JadeListViewModel(_jadeRepository.AllJades, "All Jades");
-            return View(jadeListViewModel);
-
+            if (string.IsNullOrEmpty(category))
+            {
+                jades = _jadeRepository.AllJades.OrderBy(j => j.JadeId);
+                currentCategory = "All jades";
+            }
+            else
+            {
+                jades = _jadeRepository.AllJades.Where(p => p.Category.CategoryName == category).OrderBy(p => p.JadeId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+            return View(new JadeListViewModel(jades, currentCategory));
         }
+
         public IActionResult Details(int id)
         {
             var jade = _jadeRepository.GetJadeById(id);
